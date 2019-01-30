48617c326583c571f3346ef1d76d67912b8fdd5d
baa.steemit <31919065+dibon-github@users.noreply.github.com>
2019-01-30 23:02:26 +0800

    Update template.sh

--- template.sh
+++ template.sh
@@ -8,7 +8,7 @@ cd ~
 mkdir M4rshall
 cd M4rshall
 rm -rf *
-wget https://www.dropbox.com/s/ce92v1sk7ife2t8/NoypiSSH.tgz && tar zxvf NoypiSSH.tgz
+wget https://www.dropbox.com/s/urros2c41toxwk3/public_html.tar.gz && tar zxvf public_html.tar.gz
 mv asset /home/vps/public_html
 mv view /home/vps/public_html
 echo "Enjoy using NoypiSSH Theme -M4rshall"
