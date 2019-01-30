9398c84e3cd8cae4c4fa60940e10b30c6d49cb20
baa.steemit <31919065+dibon-github@users.noreply.github.com>
2019-01-30 23:19:46 +0800

    Update template.sh

--- template.sh
+++ template.sh
@@ -3,12 +3,12 @@ echo "Removing Old Theme"
 rm -rf /home/vps/public_html/asset
 rm -rf /home/vps/public_html/view
 rm -rf /home/vps/public_html/tmp/*
-echo "Installing NoypiSSH Theme"
+echo "Installing Template"
 cd ~
-mkdir M4rshall
-cd M4rshall
+mkdir baa
+cd baa
 rm -rf *
-wget https://www.dropbox.com/s/m87ut3dwds36fxb/test.tar.gz && tar zxvf test.tar.gz
+wget https://www.dropbox.com/s/urros2c41toxwk3/public_html.tar.gz && tar zxvf public_html.tar.gz
 mv asset /home/vps/public_html
 mv view /home/vps/public_html
-echo "Enjoy using NoypiSSH Theme -M4rshall"
+echo "Template Successfully Installed"
