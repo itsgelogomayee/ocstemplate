0b0f9b4891aed5f79aa7b6d9989b2087497b5037
baa.steemit <31919065+dibon-github@users.noreply.github.com>
2019-11-04 16:07:32 +0800

    Update template.sh

--- template.sh
+++ template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Removing Old Theme"
-rm -rf /home/panel/html/asset
+rm -rf /home/panel/html/assets
 rm -rf /home/panel/html/view
 rm -rf /home/panel/html/tmp/*
 echo "Installing Template"
@@ -9,6 +9,6 @@ mkdir baa
 cd baa
 rm -rf *
 wget https://www.dropbox.com/s/iv2alrpyh3xlzk2/vpnbybaa.tar.gz && tar zxvf vpnbybaa.tar.gz
-mv -r ~/baa/assets /home/panel/html
+mv assets /home/panel/html
 mv view /home/panel/html
 echo "VPNBYBAA Template Successfully Installed"
