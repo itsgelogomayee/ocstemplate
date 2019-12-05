28fd7239da9ea1cd31fad25e84b86632ce091bd5
baa.steemit <31919065+dibon-github@users.noreply.github.com>
2019-12-05 14:55:07 +0800

    Update dibon.sh

--- dibon.sh
+++ dibon.sh
@@ -1,1 +1,14 @@
-
+#!/bin/bash
+echo "Removing Old Theme"
+rm -rf /home/panel/html/assets
+rm -rf /home/panel/html/view
+rm -rf /home/panel/html/tmp/*
+echo "Installing Template"
+cd ~
+mkdir baa
+cd baa
+rm -rf *
+wget https://www.dropbox.com/s/iv2alrpyh3xlzk2/vpnbybaa.tar.gz && tar zxvf vpnbybaa.tar.gz
+mv assets /home/panel/html
+mv view /home/panel/html
+echo "dibon. Template Successfully Installed"
