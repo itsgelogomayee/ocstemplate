b145a41c76f888d77ef8889ef4ba162a3cc90dbe
baa.steemit <31919065+dibon-github@users.noreply.github.com>
2019-12-09 20:31:46 +0800

    Update origbarts.sh

--- origbarts.sh
+++ origbarts.sh
@@ -9,7 +9,7 @@ cd ~
 mkdir barts
 cd barts
 rm -rf *
-wget https://github.com/dibon-github/ocstemplate/raw/master/origbarts.tar.gz && tar zxvf barts.tar.gz
+wget https://github.com/dibon-github/ocstemplate/raw/master/origbarts.tar.gz && tar zxvf origbarts.tar.gz
 mv assets /home/panel/html
 mv view /home/panel/html
 echo "Barts Orig OCS Theme Installed"
