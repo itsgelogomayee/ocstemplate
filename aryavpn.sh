023e41e05314e0fb08b6f955f5a7e066c26e16e1
baa.steemit <31919065+dibon-github@users.noreply.github.com>
2019-12-29 22:49:42 +0800

    Create aryavpn.sh

---
+++ aryavpn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Removing Old Theme"
+rm -rf /home/panel/html/assets
+rm -rf /home/panel/html/aryavpn
+rm -rf /home/panel/html/bonveizen
+rm -rf /home/panel/html/tmp/*
+echo "Installing Template"
+cd ~
+mkdir baa
+cd baa
+rm -rf *
+wget https://github.com/dibon-github/ocstemplate/raw/master/aryavpn.tar.gz && tar zxvf aryavpn.tar.gz
+mv aryavpn /home/panel/html
+mv bonveizen /home/panel/html
+mv view /home/panel/html
+echo "AryaVPN Template Successfully Installed"
