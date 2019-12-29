cb57f8be59d28c856bd6d41b5335f9f4dcefe989
baa.steemit <31919065+dibon-github@users.noreply.github.com>
2019-12-29 23:05:00 +0800

    Update aryavpn.sh

--- aryavpn.sh
+++ aryavpn.sh
@@ -3,6 +3,7 @@ echo "Removing Old Theme"
 rm -rf /home/panel/html/assets
 rm -rf /home/panel/html/aryavpn
 rm -rf /home/panel/html/bonveizen
+rm -rf /home/panel/html/view
 rm -rf /home/panel/html/tmp/*
 echo "Installing Template"
 cd ~
